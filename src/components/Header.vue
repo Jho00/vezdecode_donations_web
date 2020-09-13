d4ae658181f4b4867349f631cf75409c96cfeb3b
Den <denmen0207@gmail.com>
2020-09-13 18:12:08 +0300

    Add bootstrap, simple header.

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-100">
+        <div class="header-text text-center">Donations</div>
+        <div class="black-line w-100"></div>
+    </div>
+</template>
+
+<style scoped>
+    .header-text {
+        font-family: "TTCommons-DemiBold";
+        font-size: 21px;
+    }
+
+    .black-line {
+        background: #d7d8d9;
+        height: 1px;
+    }
+</style>
